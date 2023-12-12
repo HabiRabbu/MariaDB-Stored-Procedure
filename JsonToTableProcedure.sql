1f5122e458776142503dc99365587b14ef4ed847
Harvey <64276030+HabiRabbu@users.noreply.github.com>
2023-12-12 10:38:02 +0000

    Update JsonToTableProcedure.sql

--- JsonToTableProcedure.sql
+++ JsonToTableProcedure.sql
@@ -1,41 +1,41 @@
+USE Test;
+
 DELIMITER //
 
-CREATE PROCEDURE json_to_table(IN json_text TEXT)
+-- Drop the procedure if it exists (Due to working on this in console)
+DROP PROCEDURE IF EXISTS json_to_table;
+
+CREATE PROCEDURE json_to_table(json_input JSON)
 BEGIN
-    DECLARE EXIT HANDLER FOR SQLEXCEPTION
-    BEGIN
-        RESIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Invalid JSON object', MYSQL_ERRNO = 333;
-    END;
-    
-    SET @json = JSON_VALID(json_text);
-    
-    IF @json IS NULL THEN
-        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Invalid JSON object', MYSQL_ERRNO = 333;
+   DECLARE key_val_pairs JSON;
+
+    IF NOT JSON_VALID(json_input) THEN
+        SIGNAL SQLSTATE '45000'
+            SET MESSAGE_TEXT = 'Invalid JSON object', MYSQL_ERRNO = 333;
     END IF;
-    
-    SET @keys = JSON_KEYS(json_text);
-    SET @num_keys = JSON_LENGTH(@keys);
-    SET @idx = 0;
-    SET @sql = NULL;
-    
-    WHILE @idx < @num_keys DO
-        SET @key = JSON_UNQUOTE(JSON_EXTRACT(@keys, CONCAT('$[', @idx, ']')));
-        SET @value = JSON_EXTRACT(json_text, CONCAT('$.', JSON_UNQUOTE(JSON_EXTRACT(@keys, CONCAT('$[', @idx, ']')))));
-        
-        IF @idx = 0 THEN
-            SET @sql = CONCAT('SELECT "', @key, '" AS `key`, ', 'CAST(', QUOTE(JSON_UNQUOTE(@value)), ' AS CHAR) AS `value`');
-        ELSE
-            SET @sql = CONCAT(@sql, ' UNION ALL SELECT "', @key, '", ', 'CAST(', QUOTE(JSON_UNQUOTE(@value)), ' AS CHAR) AS `value`');
-        END IF;
-        
-        SET @idx = @idx + 1;
-    END WHILE;
-    
-    SET @sql = CONCAT(@sql, ';');
-    
-    PREPARE stmt FROM @sql;
-    EXECUTE stmt;
-    DEALLOCATE PREPARE stmt;
+
+    CREATE TEMPORARY TABLE IF NOT EXISTS cte_idx (idx INT);
+
+    INSERT INTO cte_idx (idx) VALUES (0);
+
+    INSERT INTO cte_idx (idx)
+    SELECT idx + 1
+    FROM cte_idx
+    WHERE idx < JSON_LENGTH(JSON_KEYS(json_input, '$')) - 1;
+    -- Create a Common Table Expression (CTE)
+    WITH recursive cte AS (
+        SELECT
+            JSON_UNQUOTE(JSON_EXTRACT(JSON_KEYS(json_input, '$'), CONCAT('$[', idx, ']'))) AS `key`,
+            JSON_UNQUOTE(JSON_EXTRACT(json_input, CONCAT('$.', JSON_UNQUOTE(JSON_EXTRACT(JSON_KEYS(json_input, '$'), CONCAT('$[', idx, ']')))))) AS `value`
+        FROM
+            cte_idx
+    WHERE idx < JSON_LENGTH(JSON_KEYS(json_input, '$'))
+    )
+    SELECT * FROM cte;
+
+    -- Drop the temporary table
+    DROP TEMPORARY TABLE IF EXISTS cte_idx;
+
 END //
 
 DELIMITER ;
